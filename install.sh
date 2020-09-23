61fef4b875b7e2b4bfae80469d6a4759111a014f
Ching Xuds <chingxuds@gmail.com>
2020-09-23 11:29:11 +0800

    提交个性化配置脚本

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+command_exists() {
+    command -v "$@" >/dev/null 2>&1
+}
+
+if command_exists curl; then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    curl -fsSLo ~/.oh-my-zsh/custom/themes/robbyrussell-custom.zsh-theme https://raw.github.com/chingxuds/ohmyzsh-custom/master/robbyrussell-custom.zsh-theme
+elif command_exists wget; then
+    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    wget https://raw.github.com/chingxuds/ohmyzsh-custom/master/robbyrussell-custom.zsh-theme -O ~/.oh-my-zsh/custom/themes/robbyrussell-custom.zsh-theme
+else
+    echo "Please install curl or wget first!"
+fi
+
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="robbyrussell-custom"' ~/.zshrc
+
+source ~/.zshrc
